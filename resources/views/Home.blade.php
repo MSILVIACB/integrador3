049874db7be25fd73bbb78bee149f9152914a233
Maria Silvia <msylviacb@gmail.com>
2021-09-06 16:44:06 -0300

    criação página Home

---
+++ resources/views/Home.blade.php
@@ -0,0 +1,162 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We"
+        crossorigin="anonymous">
+        <link rel= "stylesheet" href="/css/styles.css">
+        <script src="/js/scripts.js"></script>
+    </head>
+    <body >
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="container-fluid">
+            <div class="collapse navbar-collapse"
+            id="navbarSupportedContent">
+            <a class="navbar-link active"
+            aria-current="page"
+            href="#">
+            <img src="/img/logodynamicfit/Dynamic001.png"
+            class="d-block w-120 "
+            alt="...">
+            <button class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button></a>
+
+                <ul class="navbar-nav auto me-auto mb-2 mb-lg-0">
+
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">Planos</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">Cadastro</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">Contato</a>
+                  </li>
+              </ul>
+           </div>
+         </div>
+       </nav>
+                    <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+                    <div class="carousel-inner">
+                      <div class="carousel-item active">
+                        <img src="/img/banners/banner0.jpg" class="d-block w-100" alt="Banner">
+                      </div>
+                      <div class="carousel-item">
+                        <img src="/img/banners/banner1.jpg" class="d-block w-100" alt="Banner">
+                      </div>
+                      <div class="carousel-item">
+                        <img src="/img/banners/banner2.jpg" class="d-block w-100" alt="Banner">
+                      </div>
+                      <div class="carousel-item">
+                        <img src="/img/banners/banner3.jpg" class="d-block w-100" alt="Banner">
+                      </div>
+                      <div class="carousel-item">
+                        <img src="/img/banners/banner4.jpg" class="d-block w-100" alt="Banner">
+                      </div>
+                    </div>
+                  </div>
+                <br>
+              <h1>Escolha aquia Modalidade que mais gostar</h1>
+                    <div class="container">
+                            <div class="row justify-content-center">
+                              <div class="col-12 col-med-6 col-lg-4"> <div class="card card-center" style="width: 18rem;">
+                                <img src="/img/woman3.jpg" class="card-img-top" alt="...">
+                                <div class="card-body">
+                                  <h5 class="card-title">Musculação Feminina</h5>
+                                  <a href="#" class="btn btn-warning">Saiba Mais</a>
+                                </div>
+                              </div>
+                             </div>
+                              <div class="col-12 col-med-6 col-lg-4"> <div class="card card-center" style="width: 18rem;">
+                                <img src="/img/zumba4.jpg" class="card-img-top" alt="...">
+                                <div class="card-body">
+                                  <h5 class="card-title">Zumba</h5>
+                                  <a href="#" class="btn btn-warning">Saiba Mais</a>
+                                </div>
+                              </div> </div>
+                              <div class="col-12 col-med-6 col-lg-4"> <div class="card card-center" style="width: 18rem;">
+                                <img src="/img/saude1.jpg" class="card-img-top" alt="...">
+                                <div class="card-body">
+                                  <h5 class="card-title">Alimentação Fit</h5>
+                                  <a href="#" class="btn btn-warning">Saiba Mais</a>
+                               </div>
+                              </div>
+                             </div>
+                             <p>
+                                  <div class="col-12 col-med-6 col-lg-4"> <div class="card card-center" style="width: 18rem;">
+                                  <img src="/img/cards/boxer.jpg" class="card-img-top" alt="...">
+                                  <div class="card-body">
+                                    <h5 class="card-title">Dynamic Boxe</h5>
+                                    <a href="#" class="btn btn-warning">Saiba Mais</a>
+                                  </div>
+                                </div> </div>
+                                <div class="col-12 col-med-6 col-lg-4"> <div class="card card-center" style="width: 18rem;">
+                                  <img src="/img/cards/pilates.jpg" class="card-img-top" alt="...">
+                                  <div class="card-body">
+                                    <h5 class="card-title">Pilates</h5>
+                                    <a href="#" class="btn btn btn-warning">Saiba Mais</a>
+                                  </div>
+                                </div>
+                               </div>
+                                <div class="col-12 col-med-6 col-lg-4"> <div class="card card-center" style="width: 18rem;">
+                                  <img src="/img/cards/jump.jpg" class="card-img-top" alt="...">
+                                  <div class="card-body">
+                                    <h5 class="card-title">Dynamic Jump</h5>
+                                    <a href="#" class="btn btn-warning">Saiba Mais</a>
+                                  </div>
+                                 </div>
+                                </div>
+                              <p>
+                                <div class="col-12 col-med-6 col-lg-4"> <div class="card card-center" style="width: 18rem;">
+                                  <img src="/img/yoga2.jpg" class="card-img-top" alt="...">
+                                  <div class="card-body">
+                                    <h5 class="card-title">Yoga</h5>
+                                   <a href="#" class="btn btn-warning">Saiba Mais</a>
+                                  </div>
+                                </div> </div>
+
+                                <div class="col-12 col-med-6 col-lg-4"> <div class="card card-center" style="width: 18rem;">
+                                  <img src="/img/ritmos.jpg" class="card-img-top" alt="...">
+                                  <div class="card-body">
+                                    <h5 class="card-title">Ritmos</h5>
+                                     <a href="#" class="btn btn-warning">Saiba Mais</a>
+                                  </div>
+                                </div> </div>
+                                <div class="col-12 col-med-6 col-lg-4"> <div class="card card-center" style="width: 18rem;">
+                                  <img src="/img/man3.jpg" class="card-img-top" alt="...">
+                                  <div class="card-body">
+                                    <h5 class="card-title">Musculação Masculina</h5>
+                                    <a href="#" class="btn btn-warning">Saiba Mais</a>
+                                  </div>
+                                </div>
+                             </div>
+                              <p>
+                                <div class="col-12 col-med-6 col-lg-4"> <div class="card card-center" style="width: 18rem;">
+                                  <img src="/img/ginástica/girl5.jpg" class="card-img-top" alt="...">
+                                  <div class="card-body">
+                                    <h5 class="card-title">Ballet</h5>
+                                   <a href="#" class="btn btn-warning">Saiba Mais</a>
+                                  </div>
+                                </div> </div>
+                           </div>
+                          <br>
+                         <br>
+
+
+
+                <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js"
+                   integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj"
+                   crossorigin="anonymous">
+
+                </script>
+    </body>
+</html>

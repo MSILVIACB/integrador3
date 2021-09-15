8a36a8f3b0935cb2fda8a059951c5cc263fa1768
Maria Silvia <msylviacb@gmail.com>
2021-09-14 22:19:26 -0300

    img contato

--- resources/views/contato.blade.php
+++ resources/views/contato.blade.php
@@ -49,7 +49,7 @@
       </form>
         </div>
       <div class="col-12 col-md-6">
-        <img src="/img/machines5.jpg" class="w-100 mt-2" height="280" alt="Imagem Exemplo">
+        <img src="/img/banners/banner5.jpg" class="w-100 mt-2" height="280" alt="Imagem Exemplo">
         </div>
        </div>
        <br>

a70fcaaa111205db7d95b729e2bf47a2aaec8790
jose1113 <joseigor113@gmail.com>
2021-09-19 11:00:06 -0300

    add options

--- resources/views/cadastro.blade.php
+++ resources/views/cadastro.blade.php
@@ -1,21 +1,20 @@
 @extends ('layouts.main')
 
 @section ('content')
-
 <body>
   <div class="container-fluid">
-    <img src="/img/banners/banner2.jpg" class="img-fluid d-block w-100" alt="Banner">
+    <img src="/img/banners/banner2.jpg" class="img-fluid d-block w-100 rounded" alt="Banner">
     <h1 class="text-center mt-3">Cadastro do Aluno</h1>
   </div>
   <form class="row g-3 needs-validation m-3" novalidate>
     <div class="col-md-4">
       <label for="validationCustom01" class="form-label">Nome</label>
-      <input type="text" class="form-control" id="validationCustom01" value="Mark" required>
+      <input type="text" class="form-control" id="validationCustom01" required>
       <div class="valid-feedback">Perfeito!</div>
     </div>
     <div class="col-md-4">
       <label for="validationCustom02" class="form-label">Sobrenome</label>
-      <input type="text" class="form-control" id="validationCustom02" value="Otto" required>
+      <input type="text" class="form-control" id="validationCustom02" required>
       <div class="valid-feedback">Perfeito!</div>
     </div>
     <div class="col-md-4">
@@ -35,10 +34,33 @@
       <label for="validationCustom04" class="form-label">Estado</label>
       <select class="form-select" id="validationCustom04" required>
         <option selected disabled value="">Escolha...</option>
-        <option>SP</option>
+        <option>AC</option>
+        <option>AL</option>
+        <option>AP</option>
+        <option>AM</option>
+        <option>BA</option>
+        <option>CE</option>
+        <option>ES</option>
+        <option>GO</option>
+        <option>MA</option>
+        <option>MT</option>
+        <option>MS</option>
         <option>MG</option>
+        <option>PA</option>
+        <option>PB</option>
+        <option>PR</option>
+        <option>PE</option>
+        <option>PI</option>
         <option>RJ</option>
-        <option>Outro</option>
+        <option>RN</option>
+        <option>RS</option>
+        <option>RO</option>
+        <option>RR</option>
+        <option>SC</option>
+        <option>SP</option>
+        <option>SE</option>
+        <option>TO</option>
+        <option>DF</option>
         </select>
       <div class="invalid-feedback">Por favor, digite um nome de estado válido.</div>
     </div>
@@ -49,15 +71,14 @@
     </div>
     <div class="col-12">
       <div class="form-check">
-        <input class="form-check-input" type="checkbox" value="" id="invalidCheck" required>
+        <input class="form-check-input" type="checkbox" id="invalidCheck" required>
         <label class="form-check-label" for="invalidCheck">Por favor, concorde com os termos e condições.</label>
         <div class="invalid-feedback">Por favor, concorde antes de enviar os dados.</div>
       </div>
     </div>
     <div class="col-12">
-      <button class="btn btn-primary" type="submit">Envie os dados</button>
+      <button class="btn btn-primary" type="submit">Enviar</button>
     </div>
   </form>
 </body>
-
-    @endsection
+@endsection

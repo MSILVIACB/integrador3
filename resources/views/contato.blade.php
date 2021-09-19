328a9e7e73fc3c8a904b6ae1514a298431dfcff6
jose1113 <joseigor113@gmail.com>
2021-09-19 15:02:47 -0300

    fix form

--- resources/views/contato.blade.php
+++ resources/views/contato.blade.php
@@ -3,21 +3,21 @@
 @section ('content')
 
 <body>
-  <div class="row mt-2 m-3">
+  <div class="row mt-3 m-3 d-flex align-items-center">
     <div class="col-12 col-md-6">
       <form>
         <div class="mb-3">
-          <label for="exampleInputEmail1" class="form-label">Email </label>
-          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-          <div id="emailHelp" class="form-text">Nunca compartilharemos seu email.</div>
+          <label for="nome" class="form-label">Nome</label>
+          <input type="email" class="form-control" id="nome" aria-describedby="nome">
         </div>
         <div class="mb-3">
-          <label for="exampleInputPassword1" class="form-label">Senha</label>
-          <input type="password" class="form-control" id="exampleInputPassword1">
+          <label for="email" class="form-label">Email</label>
+          <input type="email" class="form-control" id="email" aria-describedby="emailHelp">
+          <div id="emailHelp" class="form-text">Nunca compartilharemos seu email.</div>
         </div>
-        <div class="mb-3 form-check">
-          <input type="checkbox" class="form-check-input" id="exampleCheck1">
-          <label class="form-check-label" for="exampleCheck1">Confirmar</label>
+        <div class="form-group mb-3">
+          <label for="textarea">Sua mensagem</label>
+          <textarea class="form-control" id="textarea" rows="3"></textarea>
         </div>
         <button type="submit" class="btn btn-warning">Enviar</button>
       </form>

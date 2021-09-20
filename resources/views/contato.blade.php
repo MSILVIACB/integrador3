052015486da3d60dfbf0582b0e63ba128ceb6370
Maria Silvia <msylviacb@gmail.com>
2021-09-20 15:59:46 -0300

    contato

--- resources/views/contato.blade.php
+++ resources/views/contato.blade.php
@@ -30,34 +30,41 @@
           <br>
 
           <div class="container-fluid">
-            <div class="row mt-2">
+            <div class="row mt-4">
               <div class="col-12 col-md-6">
-       <form>
-        <div class="mb-4">
+        <form>
+        <div class="mb-3">
           <label for="exampleInputEmail1" class="form-label">Email </label>
           <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
           <div id="emailHelp" class="form-text">Nunca compartilharemos seu email.</div>
         </div>
-        <div class="mb-4">
+
+        <div class="mb-3">
           <label for="exampleInputPassword1" class="form-label">Senha</label>
           <input type="password" class="form-control" id="exampleInputPassword1">
         </div>
-        <div class="mb-4 form-check">
+        <div class="mb-3 form-check">
           <input type="checkbox" class="form-check-input" id="exampleCheck1">
           <label class="form-check-label" for="exampleCheck1">Confirmar</label>
         </div>
-        <button type="submit" class="btn btn-warning">Enviar</button>
-      </form>
+
+        <div class="mb-3">
+            <label class="form-label" for="message"></label>
+            <textarea class="form-control" id="message" type="text" placeholder="Message" style="height: 10rem;" data-sb-validations="required"></textarea>
+            <div class="invalid-feedback" data-sb-feedback="message:required">Message is required.</div>
         </div>
+        <button type="submit" class="btn btn-warning" style="width: 7rem;">Enviar</button>
+       </form>
+        </div>
+
       <div class="col-12 col-md-6">
-        <img src="/img/haltere5.jpg" class="w-100 mt-2" height="280" alt="Imagem Exemplo">
+        <img src="/img/haltere5.jpg" class="w-100 mt-4"  alt="Imagem Exemplo">
         </div>
        </div>
           </div>
+         <br>
        <br>
       <br>
-        </header>
-        </body>
-
-      @endsection
-
+    </header>
+  </body>
+ @endsection

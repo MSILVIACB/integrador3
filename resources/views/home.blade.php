cbc3eb98270d8548fd55df09a4fbacd262d8633c
jose1113 <joseigor113@gmail.com>
2021-09-26 12:09:32 -0300

    fix cards

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,52 +3,27 @@
 @section ('content')
 
 <body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container-fluid px-4 px-lg-6">
-          <a class="navbar-brand" href="#!">
-            <img src="/img/logo/DynamicFit.svg" alt="Logotipo" class="w-50 rounded-2">
-          </a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarNavDropdown">
-            <ul class="navbar-nav d-flex justify-content-between">
-              <li class="nav-item">
-                <a class="nav-link" aria-current="page" href="#"><h3>Planos</h3></a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#"><h3>Cadastro</h3></a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#"><h3>Contato</h3></a>
-              </li>
-            </ul>
+  <div class="container-fluid">
+    <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img src="/img/banners/banner9.jpg" class="img-fluid" alt="Banner">
         </div>
+        <div class="carousel-item">
+          <img src="/img/banners/banner1.jpg" class="img-fluid" alt="Banner">
+        </div>
+        <div class="carousel-item">
+          <img src="/img/banners/banner2.jpg" class="img-fluid" alt="Banner">
+        </div>
+        <div class="carousel-item">
+            <img src="/img/banners/banner3.jpg" class="img-fluid" alt="Banner">
+        </div>
+        <div class="carousel-item">
+          <img src="/img/banners/banner4.jpg" class="img-fluid" alt="Banner">
+        </div>
+      </div>
     </div>
-        </nav>
-
-              <div class="container-fluid">
-                <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
-                     <div class="carousel-inner">
-                       <div class="carousel-item active">
-                         <img src="/img/banners/banner9.jpg" class="img-fluid" alt="Banner">
-                       </div>
-                       <div class="carousel-item">
-                         <img src="/img/banners/banner1.jpg" class="img-fluid" alt="Banner">
-                       </div>
-                       <div class="carousel-item">
-                         <img src="/img/banners/banner2.jpg" class="img-fluid" alt="Banner">
-                       </div>
-                       <div class="carousel-item">
-                           <img src="/img/banners/banner3.jpg" class="img-fluid" alt="Banner">
-                       </div>
-                       <div class="carousel-item">
-                         <img src="/img/banners/banner4.jpg" class="img-fluid" alt="Banner">
-                       </div>
-                     </div>
-                   </div>
-                 </div>
-<header>
+  </div>
   <div class="bg-dark py-3">
     <div class="container px-4 px-lg-5 my-5">
       <div class="text-center text-white">
@@ -60,138 +35,135 @@
 </header>
 </div>
 
-<div class="container-fluid">
-        <div class="row text-center mt-3">
-          <h1>"Escolha sua modalidade preferida"</h1>
+  <div class="container-fluid">
+    <div class="row text-center mt-3">
+      <h1>"Escolha sua modalidade preferida"</h1>
+    </div>
+  </div>
+    
+
+  <div class="container px-4 px-lg-5 mt-5">
+    <div class="row justify-content-center">
+      <div class="col-12 col-med-6 col-lg-4 d-flex align-items-stretch flex-wrap">
+        <div class="card card-center" style="max-width: 18rem;">
+          <img src="/img/cards/fitness.jpg" class="card-img-top img-fluid" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Musculação Feminina & Masculina</h5>
+            <p class="card-text">Melhora o condicionamento cardiorrespiratório, diminui a quantidade de gordura e auxilia no emagrecimento.</p>
+            <a href="#" class="btn btn-warning">Selecione</a>
+          </div>
         </div>
       </div>
-    <br>
+      <div class="col-12 col-med-6 col-lg-4 d-flex align-items-stretch flex-wrap">
+        <div class="card card-center" style="width: 18rem;">
+          <img src="/img/cards/zumba1.jpg" class="card-img-top img-fluid" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Zumba</h5>
+            <p class="card-text">Aula de fitness-dança que incorpora música latina e internacional com passos de dança combinando ritmos rápidos e lentos.</p>
+            <a href="#" class="btn btn-warning">Selecione</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-12 col-med-6 col-lg-4 d-flex align-items-stretch flex-wrap">
+        <div class="card card-center" style="width: 18rem;">
+          <img src="/img/cards/girl5.jpg" class="card-img-top img-fluid" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Ballet</h5>
+            <p class="card-text">Auxilia no autoconhecimento. Beneficia a elasticidade, postura, equilíbrio, respiração, emagrecimento e definição muscular.</p>
+            <a href="#" class="btn btn-warning">Selecione</a>
+          </div>
+        </div>
+      </div>
+    </div>
 
-    <div class="container px-4 px-lg-5 mt-5">
-                  <div class="row justify-content-center">
-                   <div class="col-12 col-med-6 col-lg-4">
-                      <div class="card card-center" style="max-width: 18rem;">
-                        <img src="/img/cards/fitness.jpg" class="card-img-top img-fluid" alt="...">
-                        <div class="card-body">
-                          <h5 class="card-title">Musculação Feminina & Masculina</h5>
-                          <p class="card-text">Melhora o condicionamento cardiorrespiratório, diminui a quantidade de gordura e auxilia no emagrecimento.</p>
-                          <a href="#" class="btn btn-warning">Selecione</a>
-                        </div>
-                      </div>
-                    </div>
-                    <div class="col-12 col-med-6 col-lg-4">
-                      <div class="card card-center" style="width: 18rem;">
-                        <img src="/img/cards/zumba1.jpg" class="card-img-top img-fluid" alt="...">
-                        <div class="card-body">
-                          <h5 class="card-title">Zumba</h5>
-                          <p class="card-text">Aula de fitness-dança que incorpora música latina e internacional com passos de dança combinando ritmos rápidos e lentos.</p>
-                          <a href="#" class="btn btn-warning">Selecione</a>
-                        </div>
-                    </div>
-                   </div>
-                <div class="col-12 col-med-6 col-lg-4">
-                  <div class="card card-center" style="width: 18rem;">
-                      <img src="/img/cards/girl5.jpg" class="card-img-top img-fluid" alt="...">
-                      <div class="card-body">
-                        <h5 class="card-title">Ballet</h5>
-                        <p class="card-text">Auxilia no autoconhecimento. Beneficia a elasticidade, postura, equilíbrio, respiração, emagrecimento e definição muscular.</p>
-                        <a href="#" class="btn btn-warning">Selecione</a>
-                      </div>
-                    </div>
-                  </div>
-                  </div>
-                  <br>
-                  <br>
-
-                  <div class="row justify-content-center">
-                  <div class="col-12 col-med-6 col-lg-4">
-                    <div class="card card-center" style="width: 18rem;">
-                      <img src="/img/cards/karate.jpg" class="card-img-top img-fluid" alt="...">
-                      <div class="card-body">
-                        <h5 class="card-title">Dynamic Combat</h5>
-                        <p class="card-text">Programa baseado nos mais diversos estilos de Artes Marciais como: Boxe, Karatê, Muay Thai, Jiu Jitsu, Capoeira e Kick Boxing. Desenvolve o condicionamento físico.</p>
-                        <a href="#" class="btn btn-warning">Selecione</a>
-                      </div>
-                    </div>
-                  </div>
-                      <div class="col-12 col-med-6 col-lg-4">
-                        <div class="card card-center" style="width: 18rem;">
-                          <img src="/img/cards/pilates.jpg" class="card-img-top img-fluid" alt="...">
-                          <div class="card-body">
-                            <h5 class="card-title">Pilates</h5>
-                            <p class="card-text">A aula mescla exercícios de flexibilidade e força a partir do trabalho da musculatura envolvendo nosso centro de gravidade e equilíbrio..</p>
-                            <a href="#" class="btn btn btn-warning">Selecione</a>
-                          </div>
-                      </div>
-                      </div>
-                      <div class="col-12 col-med-6 col-lg-4">
-                          <div class="card card-center" style="width: 18rem;">
-                        <img src="/img/cards/jump.jpg" class="card-img-top img-fluid" alt="...">
-                        <div class="card-body">
-                          <h5 class="card-title">Dynamic Jump</h5>
-                          <p class="card-text">Aula com mini-trampolim e a combinação entre músicas e movimentos através de coreografias. Proporcionam o trabalho cardio, equilíbrio e diversão.</p>
-                          <a href="#" class="btn btn-warning">Selecione</a>
-                        </div>
-                        </div>
-                      </div>
-                    </div>
-                    <br>
-                    <br>
-                     <div class="row justify-content-center">
-                      <div class="col-12 col-med-6 col-lg-4">
-                          <div class="card card-center" style="width: 18rem;">
-                        <img src="/img/cards/yoga2.jpg" class="card-img-top img-fluid" alt="...">
-                        <div class="card-body">
-                          <h5 class="card-title">Yoga</h5>
-                          <p class="card-text">Prática milenar com benefícios físicos e mentais. Proporciona redução do estresse, alívio da ansiedade, melhora da concentração, força, flexibilidade e gasto calórico</p>
-                          <a href="#" class="btn btn-warning">Selecione</a>
-                        </div>
-                      </div>
-                     </div>
-                     <div class="col-12 col-med-6 col-lg-4">
-                        <div class="card card-center" style="width: 18rem;">
-                          <img src="/img/cards/ritmos.jpg" class="card-img-top img-fluid" alt="...">
-                          <div class="card-body">
-                            <h5 class="card-title">Ritmos</h5>
-                            <p class="card-text">Aulas de dança fitness ao som de vários estilos musicais com coreografias criadas pelo professor da aula. Trabalha a coordenação, reduzo estresse e a ansiedade.</p>
-                            <a href="#" class="btn btn-warning">Selecione</a>
-                          </div>
-                        </div>
-                      </div>
-                      <div class="col-12 col-med-6 col-lg-4">
-                        <div class="card card-center" style="width: 18rem;">
-                          <img src="/img/kickboxing.jpg" class="card-img-top img-fluid" alt="...">
-                          <div class="card-body">
-                            <h5 class="card-title">Dynamic Boxe</h5>
-                            <p class="card-text">Aula de boxe-fitness que desenvolve o condicionamento físico pois trabalhacom vários grupos musculares através da técnica e exercícios de boxe.</p>
-                            <a href="#" class="btn btn-warning">Selecione</a>
-                          </div>
-                      </div>
-                    </div>
-                  </div>
-              </div>
-            <br>
-            <br>
-                <div class="container-fluid">
-                  <div class="row mt-2">
-                    <div class="col-12 col-md-8">
-                      <form>
-                        <div class="mb-3">
-                          <label for="exampleInputEmail1" class="form-label">Email</label>
-                          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                        </div>
-                        <div class="mb-3">
-                          <label for="exampleInputPassword1" class="form-label">Senha</label>
-                          <input type="password" class="form-control" id="exampleInputPassword1">
-                        </div>
-                        <button type="submit" class="btn btn-warning">Login</button>
-                      </form>
-                    </div>
-                    <div class="col-12 col-md-4">
-                      <img src="/img/workout.jpg" class="w-100 mt-2 " height="300" alt="Imagem Exemplo">
-                    </div>
-                  </div>
-                </div>
-                <br>
-            </body>
-            @endsection
+    <div class="row justify-content-center d-flex align-items-stretch flex-wrap">
+      <div class="col-12 col-med-6 col-lg-4">
+        <div class="card card-center" style="width: 18rem;">
+          <img src="/img/cards/karate.jpg" class="card-img-top img-fluid" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Dynamic Combat</h5>
+            <p class="card-text">Programa baseado nos mais diversos estilos de Artes Marciais como: Boxe, Karatê, Muay Thai, Jiu Jitsu, Capoeira e Kick Boxing. Desenvolve o condicionamento físico.</p>
+            <a href="#" class="btn btn-warning">Selecione</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-12 col-med-6 col-lg-4 d-flex align-items-stretch flex-wrap">
+        <div class="card card-center" style="width: 18rem;">
+          <img src="/img/cards/pilates.jpg" class="card-img-top img-fluid" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Pilates</h5>
+            <p class="card-text">A aula mescla exercícios de flexibilidade e força a partir do trabalho da musculatura envolvendo nosso centro de gravidade e equilíbrio..</p>
+            <a href="#" class="btn btn btn-warning">Selecione</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-12 col-med-6 col-lg-4 d-flex align-items-stretch flex-wrap">
+        <div class="card card-center" style="width: 18rem;">
+          <img src="/img/cards/jump.jpg" class="card-img-top img-fluid" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Dynamic Jump</h5>
+            <p class="card-text">Aula com mini-trampolim e a combinação entre músicas e movimentos através de coreografias. Proporcionam o trabalho cardio, equilíbrio e diversão.</p>
+            <a href="#" class="btn btn-warning">Selecione</a>
+          </div>
+        </div>
+      </div>
+    </div>
+                               
+    <div class="row justify-content-center d-flex align-items-stretch flex-wrap">
+      <div class="col-12 col-med-6 col-lg-4">
+        <div class="card card-center" style="width: 18rem;">
+          <img src="/img/cards/yoga2.jpg" class="card-img-top img-fluid" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Yoga</h5>
+            <p class="card-text">Prática milenar com benefícios físicos e mentais. Proporciona redução do estresse, alívio da ansiedade, melhora da concentração, força, flexibilidade e gasto calórico</p>
+            <a href="#" class="btn btn-warning">Selecione</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-12 col-med-6 col-lg-4 d-flex align-items-stretch flex-wrap">
+        <div class="card card-center" style="width: 18rem;">
+          <img src="/img/cards/ritmos.jpg" class="card-img-top img-fluid" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Ritmos</h5>
+            <p class="card-text">Aulas de dança fitness ao som de vários estilos musicais com coreografias criadas pelo professor da aula. Trabalha a coordenação, reduzo estresse e a ansiedade.</p>
+            <a href="#" class="btn btn-warning">Selecione</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-12 col-med-6 col-lg-4 d-flex align-items-stretch flex-wrap">
+        <div class="card card-center" style="width: 18rem;">
+          <img src="/img/kickboxing.jpg" class="card-img-top img-fluid" alt="...">
+          <div class="card-body d-flex align-items-strech flex-wrap">
+            <h5 class="card-title">Dynamic Boxe</h5>
+            <p class="card-text">Aula de boxe-fitness que desenvolve o condicionamento físico pois trabalhacom vários grupos musculares através da técnica e exercícios de boxe.</p>
+            <div>
+              <a href="#" class="btn btn-warning">Selecione</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+            
+  <div class="container-fluid">
+    <div class="row mt-2">
+      <div class="col-12 col-md-8">
+        <form>
+          <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Email</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+          </div>
+          <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label">Senha</label>
+            <input type="password" class="form-control" id="exampleInputPassword1">
+          </div>
+          <button type="submit" class="btn btn-warning">Login</button>
+        </form>
+      </div>
+      <div class="col-12 col-md-4">
+        <img src="/img/workout.jpg" class="w-100 mt-2 " height="300" alt="Imagem Exemplo">
+      </div>
+    </div>
+  </div>
+</body>
+@endsection
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -7,26 +7,49 @@
         <link rel="stylesheet" href="/css/styles.css">
         <link rel="stylesheet" href="css/font-awesome.min.css">
         <script src="/js/scripts.js"></script>
-
     </head>
 
     <body>
-    @yield('content')
+        <header>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="container-fluid px-4 px-lg-6">
+                <a class="navbar-brand" href="/">
+                    <img src="/img/logo/DynamicFit.svg" alt="Logotipo" class="w-50 rounded-2">
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                    <ul class="navbar-nav d-flex justify-content-between">
+                        <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="#"><h3>Planos</h3></a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link" href="#"><h3>Cadastro</h3></a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link" href="#"><h3>Contato</h3></a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        </header>
 
-    <footer class="py-3 bg-dark">
-    <div class="container">
-    <p class="m-0 text-center text-warning">Copyright &copy; Dynamic Fit Website 2021</p>
-    <div class="social-links">
-        <a href="#" class="text-warning me-2"><i class="fab fa-facebook-f"></i></a>
-        <a href="#" class="text-warning me-2"><i class="fab fa-instagram"></i></a>
-        <a href="#" class="text-warning me-2"><i class="fab fa-twitter"></i></a>
-        <a href="#" class="text-warning me-2"><i class="fab fa-whatsapp"></i></a>
-      </div>
-</div>
-    </footer>
+        @yield('content')
 
+        <footer class="py-3 bg-dark">
+            <div class="container">
+                <p class="m-0 text-center text-warning">Copyright &copy; Dynamic Fit Website 2021</p>
+                <div class="social-links">
+                    <a href="#" class="text-warning me-2"><i class="fab fa-facebook-f"></i></a>
+                    <a href="#" class="text-warning me-2"><i class="fab fa-instagram"></i></a>
+                    <a href="#" class="text-warning me-2"><i class="fab fa-twitter"></i></a>
+                    <a href="#" class="text-warning me-2"><i class="fab fa-whatsapp"></i></a>
+                </div>
+            </div>
+        </footer>
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
-
-</body>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
+    </body>
 </html>

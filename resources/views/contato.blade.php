2c320314c604b1a1d8ec527e093c1180b10f1e49
jose1113 <joseigor113@gmail.com>
2021-09-26 14:09:11 -0300

    fix footer

--- resources/views/contato.blade.php
+++ resources/views/contato.blade.php
@@ -2,69 +2,36 @@
 
 @section ('content')
 
-<body >
-    <header>
-      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container-fluid px-4 px-lg-5">
-          <a class="navbar-brand" href="#!">
-            <img src="/img/logo/DynamicFit.svg" alt="Logotipo" class="w-50 rounded-2">
-          </a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarNavDropdown">
-            <ul class="navbar-nav d-flex justify-content-between">
-              <li class="nav-item">
-                <a class="nav-link" aria-current="page" href="#"><h3>Planos</h3></a>
-                      </li>
-                      <li class="nav-item">
-                        <a class="nav-link" href="#"><h3>Cadastro</h3></a>
-                      </li>
-                      <li class="nav-item">
-                        <a class="nav-link" href="#"><h3>Contato</h3></a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-          <br>
-
-          <div class="container-fluid">
-            <div class="row mt-4">
-              <div class="col-12 col-md-6">
+<body>
+  <div class="container-fluid">
+    <div class="row mt-4">
+      <div class="col-12 col-md-6">
         <form>
-        <div class="mb-3">
-          <label for="exampleInputEmail1" class="form-label">Email </label>
-          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-          <div id="emailHelp" class="form-text">Nunca compartilharemos seu email.</div>
-        </div>
-
-        <div class="mb-3">
-          <label for="exampleInputPassword1" class="form-label">Senha</label>
-          <input type="password" class="form-control" id="exampleInputPassword1">
-        </div>
-        <div class="mb-3 form-check">
-          <input type="checkbox" class="form-check-input" id="exampleCheck1">
-          <label class="form-check-label" for="exampleCheck1">Confirmar</label>
-        </div>
-
-        <div class="mb-3">
+          <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Email </label>
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+            <div id="emailHelp" class="form-text">Nunca compartilharemos seu email.</div>
+          </div>
+          <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label">Senha</label>
+            <input type="password" class="form-control" id="exampleInputPassword1">
+          </div>
+          <div class="mb-3 form-check">
+            <input type="checkbox" class="form-check-input" id="exampleCheck1">
+            <label class="form-check-label" for="exampleCheck1">Confirmar</label>
+          </div>
+          <div class="mb-3">
             <label class="form-label" for="message"></label>
             <textarea class="form-control" id="message" type="text" placeholder="Message" style="height: 10rem;" data-sb-validations="required"></textarea>
             <div class="invalid-feedback" data-sb-feedback="message:required">Message is required.</div>
-        </div>
-        <button type="submit" class="btn btn-warning" style="width: 7rem;">Enviar</button>
-       </form>
-        </div>
-
+          </div>
+          <button type="submit" class="btn btn-warning" style="width: 7rem;">Enviar</button>
+        </form>
+      </div>
       <div class="col-12 col-md-6">
         <img src="/img/haltere5.jpg" class="w-100 mt-4"  alt="Imagem Exemplo">
-        </div>
-       </div>
-          </div>
-         <br>
-       <br>
-      <br>
-    </header>
-  </body>
- @endsection
+      </div>
+    </div>
+  </div>
+</body>
+@endsection
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -14,7 +14,7 @@
             <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
                 <div class="container-fluid px-4 px-lg-6">
                     <a class="navbar-brand" href="/">
-                        <img src="/img/logo/DynamicFit.svg" alt="Logotipo" class="w-50 rounded-2">
+                        <img src="/img/logo/DynamicFit.svg" alt="Logotipo" class="w-50 rounded-2 img-fluid">
                     </a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>

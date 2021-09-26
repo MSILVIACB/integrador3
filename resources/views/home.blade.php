eb8216496c474ff583045c7835b604518598855a
jose1113 <joseigor113@gmail.com>
2021-09-26 13:49:28 -0300

    fix login on home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -144,22 +144,22 @@
     </div>
   </div>
             
-  <div class="container m-3">
-    <div class="row mt-2">
+  <div class="container m-3 mt-2">
+    <div class="row">
       <div class="col-12 col-md-8">
         <form>
-          <div class="mb-3">
+          <div class="col-auto w-auto col-12 col-md-8 mb-3">
             <label for="exampleInputEmail1" class="form-label">Email</label>
             <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
           </div>
-          <div class="mb-3">
+          <div class="mb-3 col-auto w-auto">
             <label for="exampleInputPassword1" class="form-label">Senha</label>
             <input type="password" class="form-control" id="exampleInputPassword1">
           </div>
-          <button type="submit" class="btn btn-warning">Login</button>
+          <button type="submit" class="btn btn-warning mb-2">Login</button>
         </form>
       </div>
-      <div class="col-12 col-md-4">
+      <div class="col-12 col-md-4 d-none d-sm-block">
         <img src="/img/workout.jpg" class="w-100 rounded" height="300" alt="Imagem Exemplo">
       </div>
     </div>
